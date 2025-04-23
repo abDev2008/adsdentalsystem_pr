443ec77f25a0cf4eab8c2a776f3658b585d34f7c
Abel h <abelhailu320@gmail.com>
2025-04-23 01:01:23 -0500

    first commit

---
+++ src/main/java/com/abletocode/adsdentalsystem/AdsdentalsystemApplication.java
@@ -0,0 +1,13 @@
+package com.abletocode.adsdentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsdentalsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsdentalsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abletocode/adsdentalsystem/AdsdentalsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abletocode.adsdentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsdentalsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
